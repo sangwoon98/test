0149a21067375c191a22d5c352e2be1563cf2754
sangwoon98 <dbstkddns123@naver.com>
2023-03-29 16:47:21 +0900

    ha

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sangwoon/test/venv"
+set -gx VIRTUAL_ENV "/Users/sangwoon/Programming/test/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
